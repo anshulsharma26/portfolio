5edb3b3ee898b3baf9bb9b279b0851569af949e0
anshulsharma26 <76639094+anshulsharma26@users.noreply.github.com>
2021-06-23 16:57:46 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', ()=>{
+   //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
+var i = 0;
+var txt = 'Hi, I am Anshul Sharma';
+var speed = 250;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter()
