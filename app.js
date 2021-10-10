2f23553617989230635556c569c46738461f43e3
anshulsharma26 <76639094+anshulsharma26@users.noreply.github.com>
2021-10-10 21:30:24 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const preloader = document.getElementById('loader');
+function myFunction(){
+  preloader.style.display="none";
+
+}
+
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
